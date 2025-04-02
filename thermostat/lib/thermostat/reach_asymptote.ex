0a86b82517fe62e720e8a86fe83566f9f47db44c
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2025-04-02 14:55:15 -0500

    asymptotic approach works

--- thermostat/lib/thermostat/reach_asymptote.ex
+++ thermostat/lib/thermostat/reach_asymptote.ex
@@ -3,14 +3,14 @@ defmodule Thermostat.ReachAsymptote do
 
   def new(start_value, asymptote, rate)
       when is_float(start_value) and is_float(asymptote) and is_float(rate) do
-    {s, us} = NaiveDateTime.utc_now() |> NaiveDateTime.to_gregorian_seconds()
-    start_time = s + us / 1_000_000.0
+    {seconds, microseconds} = NaiveDateTime.utc_now() |> NaiveDateTime.to_gregorian_seconds()
+    start_time = seconds + microseconds / 1_000_000.0
     %__MODULE__{t0: start_time, v0: start_value, vf: asymptote, r: rate, v: start_value}
   end
 
   def value_now(%__MODULE__{} = state) do
-    {s, us} = NaiveDateTime.utc_now() |> NaiveDateTime.to_gregorian_seconds()
-    t = s + us / 1_000_000.0
+    {seconds, microseconds} = NaiveDateTime.utc_now() |> NaiveDateTime.to_gregorian_seconds()
+    t = seconds + microseconds / 1_000_000.0
     v = state.vf - (state.vf - state.v0) * :math.exp(-state.r * (t - state.t0))
     %{state | v: v}
   end
--- thermostat/lib/thermostat_web/live/thermostat_live.ex
+++ thermostat/lib/thermostat_web/live/thermostat_live.ex
@@ -5,8 +5,8 @@ defmodule ThermostatWeb.ThermostatLive do
     ~H"""
     <div>
       <h1>Thermostat</h1>
-      <p>Current Temperature: {@current_temperature}°C</p>
-      <p>Desired Temperature: {@desired_temperature}°C</p>
+      <p>Current Temperature: {Float.round(@state.v, 1)}°C</p>
+      <p>Desired Temperature: {@state.vf}°C</p>
       <div class="text-4xl">
         <button phx-click="increase" class="px-4 py-2 bg-red-500 text-white rounded">+</button>
         <button phx-click="decrease" class="px-4 py-2 bg-blue-500 text-white rounded">-</button>
@@ -18,35 +18,27 @@ defmodule ThermostatWeb.ThermostatLive do
   def mount(_params, _session, socket) do
     if connected?(socket), do: :timer.send_interval(100, self(), :tick)
 
-    state =
-      {:ok, assign(socket, current_temperature: 18.0, desired_temperature: 18.0)}
+    state = Thermostat.ReachAsymptote.new(30.0, 22.0, 0.3)
+    {:ok, assign(socket, state: state)}
   end
 
   def handle_info(:tick, socket) do
-    now = NaiveDateTime.utc_now() |> NaiveDateTime.to_gregorian_seconds()
+    # now = NaiveDateTime.utc_now() |> NaiveDateTime.to_gregorian_seconds()
+    new_state = Thermostat.ReachAsymptote.value_now(socket.assigns.state)
 
-    current_temperature =
-      cond do
-        socket.assigns.current_temperature < socket.assigns.desired_temperature ->
-          socket.assigns.current_temperature + 0.1
-
-        socket.assigns.current_temperature > socket.assigns.desired_temperature ->
-          socket.assigns.current_temperature - 0.1
-
-        true ->
-          socket.assigns.current_temperature
-      end
-
-    {:noreply, assign(socket, current_temperature: Float.round(current_temperature, 1))}
+    {:noreply, assign(socket, state: new_state)}
   end
 
   def handle_event("increase", _params, socket) do
-    new_temperature = socket.assigns.desired_temperature + 1.0
-    {:noreply, assign(socket, desired_temperature: new_temperature)}
+    {:noreply, assign(socket, state: update_state(socket.assigns.state, 1.0))}
   end
 
   def handle_event("decrease", _params, socket) do
-    new_temperature = socket.assigns.desired_temperature - 1.0
-    {:noreply, assign(socket, desired_temperature: new_temperature)}
+    {:noreply, assign(socket, state: update_state(socket.assigns.state, -1.0))}
+  end
+
+  def update_state(%Thermostat.ReachAsymptote{} = state, delta) do
+    new_vf = state.vf + delta
+    Thermostat.ReachAsymptote.new(state.v, new_vf, state.r)
   end
 end
