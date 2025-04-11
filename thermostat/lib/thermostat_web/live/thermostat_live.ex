e9a59ca9b74f5013e82b96927825e74a5b4a5016
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2025-04-11 11:04:48 -0400

    modest UI changes

--- thermostat/lib/thermostat_web/live/thermostat_live.ex
+++ thermostat/lib/thermostat_web/live/thermostat_live.ex
@@ -7,27 +7,35 @@ defmodule ThermostatWeb.ThermostatLive do
       <h1>Thermostat</h1>
       <h2>Current Temperature: {Float.round(@state.v, 1)}°C</h2>
       <h2>Desired Temperature: {@state.vf}°C</h2>
-      <div class="text-4xl">
+      <div class="text-6xl">
         <button phx-click="increase" class="px-4 py-2 bg-red-500 text-white rounded">+</button>
-        Target: {@state.vf}°C. Now: {Float.round(@state.v, 1)}°C.
-        <button phx-click="decrease" class="px-4 py-2 bg-blue-500 text-white rounded">-</button>
+        <div class="text-3xl">Target: {@state.vf}°C. Now: {Float.round(@state.v, 1)}°C.</div>
+        <button phx-click="decrease" class="px-5 py-2 bg-blue-500 text-white rounded">-</button>
+        <p>Count: {@count}</p>
+        <p>Count/s: {Float.round(@count / @tocks, 0)}</p>
+        <p>Tocks: {@tocks}</p>
       </div>
     </div>
     """
   end
 
   def mount(_params, _session, socket) do
-    if connected?(socket), do: :timer.send_interval(500, self(), :tick)
-
-    state = Thermostat.ReachAsymptote.new(30.0, 22.0, 0.3)
-    {:ok, assign(socket, state: state)}
+    if connected?(socket), do: :timer.send_interval(100, self(), :tick)
+    if connected?(socket), do: :timer.send_interval(1000, self(), :tock)
+    state = Thermostat.ReachAsymptote.new(30.0, 22.0, 0.075)
+    {:ok, assign(socket, state: state, count: 1, tocks: 1)}
   end
 
   def handle_info(:tick, socket) do
     # now = NaiveDateTime.utc_now() |> NaiveDateTime.to_gregorian_seconds()
     new_state = Thermostat.ReachAsymptote.value_now(socket.assigns.state)
+    {:noreply, assign(socket, state: new_state, count: socket.assigns.count + 1)}
+  end
 
-    {:noreply, assign(socket, state: new_state)}
+  def handle_info(:tock, socket) do
+    # now = NaiveDateTime.utc_now() |> NaiveDateTime.to_gregorian_seconds()
+    new_state = Thermostat.ReachAsymptote.value_now(socket.assigns.state)
+    {:noreply, assign(socket, state: new_state, tocks: socket.assigns.tocks + 1)}
   end
 
   def handle_event("increase", _params, socket) do
